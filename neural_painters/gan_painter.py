31b5c824c606579d5adf84fd4d58011567cb049f
Reiichiro Nakano <reiichiro.s.nakano@gmail.com>
2019-12-02 00:57:11 +0900

    reconsutrction loss to GAN training

--- neural_painters/gan_painter.py
+++ neural_painters/gan_painter.py
@@ -239,7 +239,12 @@ def train_gan_neural_painter(action_size: int,
 
         if use_reconstruction_loss and batch_idx < stop_reconstruction_loss_after_n_steps:
           uneven_mult = 10. if batch_idx < 100000 else 1.  # Do I need this?
-          reconstruction_loss_mult = 10. if batch_idx < 500000 else 10**(-(batch_idx//50000)+10)
+          if batch_idx < 500000:
+            reconstruction_loss_mult = 10.
+          elif batch_idx < 550000:
+            reconstruction_loss_mult = 1.
+          else:
+            reconstruction_loss_mult = 0.1
           reconstruction_loss, _ = reconstruction_loss_function(generated, strokes, uneven_mult)
           generator_loss += reconstruction_loss_mult*reconstruction_loss
           writer.add_scalar('reconstruction_loss', reconstruction_loss, batch_idx)
